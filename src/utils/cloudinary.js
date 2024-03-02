97174ce8bd892f762f704ba60cf990f3f20f71f7
rutul-kulkarni <rutulkulkarni8380@gmail.com>
2024-03-02 16:14:41 +0530

    multer middleware added

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,31 @@
+import fs from "fs";
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    //upload file to cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+
+    console.log("uploaded to cloudinary", response);
+    return response;
+  } catch (err) {
+    fs.unlinkSync(localFilePath); //remove locally saved file on failed upload
+  }
+};
+
+cloudinary.uploader.upload(
+  "https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+  { public_id: "olympic_flag" },
+  function (error, result) {
+    console.log(result);
+  }
+);
